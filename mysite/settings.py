eae326b51592e9f36695e051ea0a6311f21d5d41
KaioAntonio <kaioaar@gmail.com>
2022-02-16 23:18:22 -0300

    Seção Sobre mim

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%8=m!vl(mj#$x(5u#8mayv50g5#kt9u39=30n!@4)^dx(2@bw)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.port_page, name='index'),
 ]
 
