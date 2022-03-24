7002271f73b736351c23fe69cc2088b8090b2061
Kaio Antônio <kaioaar@gmail.com>
2022-03-24 17:07:53 -0300

    Portfolio novo 2.0

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
 def port_page(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
