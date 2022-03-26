6a0b6265d6c0ba7e31e23a51737b5d66565d03f3
Kaio Antônio <kaioaar@gmail.com>
2022-03-25 23:42:30 -0300

    Section Habilidades

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,4 +1,4 @@
-
+/* Abre e fecha menu lateral */
 
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
@@ -8,4 +8,18 @@ menuMobile.addEventListener('click', () =>{
     ? menuMobile.classList.replace("bi-list","bi-x")
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
+})
+
+
+/* fechar o menu */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
 })
